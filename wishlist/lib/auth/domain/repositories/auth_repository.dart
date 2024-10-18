763265a8d9ae9b2b796a11ec3c699fdfd632fe60
babirms <bramos@id.uff.br>
2024-10-18 13:50:35 -0300

    feat: create cubit files

--- wishlist/lib/auth/domain/repositories/auth_repository.dart
+++ wishlist/lib/auth/domain/repositories/auth_repository.dart
@@ -18,10 +18,8 @@ class AuthRepository {
     );
   }
 
-  Future<UserEntity> login({
-    required String email,
-    required String password,
-  }) async {
+  Future<UserEntity> login(
+      {required String email, required String password}) async {
     /// Realiza o login no FirebaseAuth
     final success = await authRemoteDataSource.loginWithEmailAndPassword(
       email: email,
