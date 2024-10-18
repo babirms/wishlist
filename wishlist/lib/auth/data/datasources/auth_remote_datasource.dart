4f0b2d7d5c5e0779445a13079ff70e829b9a5c03
babirms <bramos@id.uff.br>
2024-10-18 13:43:05 -0300

    feat: update AuthRemoteDataSource and create AuthRepository

--- wishlist/lib/auth/data/datasources/auth_remote_datasource.dart
+++ wishlist/lib/auth/data/datasources/auth_remote_datasource.dart
@@ -1,1 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:wishlist/auth/data/entities/user_entity.dart';
 
+class AuthRemoteDataSource {
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firestore;
+
+  AuthRemoteDataSource({
+    required FirebaseAuth firebaseAuth,
+    required FirebaseFirestore firestore,
+  })  : _firebaseAuth = firebaseAuth,
+        _firestore = firestore;
+
+  Future<void> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      User? user = userCredential.user;
+
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'id': user.uid,
+          'email': email,
+          'name': name,
+          'createdAt': Timestamp.now(),
+        });
+      }
+    } catch (e) {
+      throw Exception('Erro ao criar usuário: $e');
+    }
+  }
+
+  Future<bool> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user != null;
+    } catch (e) {
+      throw Exception('Erro ao fazer login: $e');
+    }
+  }
+
+  Future<void> sendPasswordResetEmail({required String email}) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      throw Exception('Erro ao enviar e-mail de recuperação de senha: $e');
+    }
+  }
+
+  Future<UserEntity?> getUserByEmail(String email) async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore
+          .collection('users')
+          .where('email', isEqualTo: email)
+          .limit(1)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final userData =
+            querySnapshot.docs.first.data() as Map<String, dynamic>;
+        return UserEntity.fromJson(userData);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      throw Exception('Erro ao buscar usuário pelo email: $e');
+    }
+  }
+}
---
+++ wishlist/lib/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,46 @@
+import 'package:wishlist/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:wishlist/auth/data/entities/user_entity.dart';
+
+class AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepository({required this.authRemoteDataSource});
+
+  Future<void> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    return await authRemoteDataSource.signUpWithEmailAndPassword(
+      email: email,
+      password: password,
+      name: name,
+    );
+  }
+
+  Future<UserEntity> login({
+    required String email,
+    required String password,
+  }) async {
+    /// Realiza o login no FirebaseAuth
+    final success = await authRemoteDataSource.loginWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    if (success) {
+      final user = await authRemoteDataSource.getUserByEmail(email);
+      if (user != null) {
+        return user;
+      } else {
+        throw Exception('Usuário não existente na base');
+      }
+    } else {
+      throw Exception('Erro ao fazer login');
+    }
+  }
+
+  Future<void> sendPasswordResetEmail({required String email}) async {
+    return await authRemoteDataSource.sendPasswordResetEmail(email: email);
+  }
+}
