4bb08e759184d4f48bf4174521ed3b103405b747
babirms <bramos@id.uff.br>
2024-10-19 22:48:00 -0300

    feat: create wishlist domain folder file

---
+++ wishlist/lib/features/wishlist/domain/repositories/wishlist_repository.dart
@@ -0,0 +1,24 @@
+import 'package:wishlist/features/products/data/entities/product_entity.dart';
+import 'package:wishlist/features/wishlist/data/datasources/wishlist_remote_datasource.dart';
+import 'package:wishlist/features/wishlist/data/entities/wishlist_entity.dart';
+
+class WishlistRepository {
+  final WishlistRemoteDataSource remoteDataSource;
+
+  WishlistRepository({required this.remoteDataSource});
+
+  Future<WishlistEntity?> getWishlistFromUserId(
+      {required String userId}) async {
+    return await remoteDataSource.getWishlistFromUserId(userId: userId);
+  }
+
+  Future<void> createWishlist(
+      {required String userId, List<ProductEntity>? productList}) async {
+    return await remoteDataSource.createWishlist(
+        userId: userId, productList: productList);
+  }
+
+  Future<void> updateWishlist({required WishlistEntity wishlist}) async {
+    return await remoteDataSource.updateWishlist(wishlist: wishlist);
+  }
+}
